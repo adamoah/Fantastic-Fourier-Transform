5078c8408e2ca6ee59fd160605dc78a3cc59f25b
Andrews Damoah <goldenblade244@gmail.com>
2025-04-21 22:33:44 -0400

    updated visualization

--- functions/test.py
+++ functions/test.py
@@ -1,2 +1,47 @@
-def func():
-    return "Hello World"
+import numpy as np
+import cv2
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import time
+
+
+def load():
+
+    # x and y data
+    x = np.arange(0, 20, 0.1)
+    y = np.cos(x)
+    
+    df = pd.DataFrame({'x': x, 'y': y}) # create dataframe
+
+    fig = px.line(df, x='x', y='y') # basic visualization
+
+    steps = []
+    for f in range(0, 51):  # Add frequencies from 0 to 5 (in 0.1 intervals)
+        step = {
+            "method": "update",
+            "args": [{"y": [np.sin(0.1 * f * x)]}],
+            "label": str(f*0.1),
+        }
+        steps.append(step)
+
+    # Create the slider
+    sliders = [
+        {
+            "active": 0,
+            "pad": {"t": 50},
+            "steps": steps,
+            "currentvalue": {"prefix": "Frequency: "},
+        }
+    ]
+
+    # Update the layout with the slider
+    fig.update_layout(sliders=sliders)
+
+    return fig # retrun vis
+
+    
+
+
+
+    
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import numpy as np
 import streamlit as st
+import streamlit.components.v1 as components
 import cv2 as cv
 import matplotlib.pyplot as plt
-from functions.test import func
+from functions.test import *
 
 def main():
     '''
     main function displays the webside
     '''
     st.title("Hello World")
-    st.write(func())
+    # value = st.slider("Brightness", 0, 10, value=1, step=1)
+    fig = load()
+    st.plotly_chart(fig)
 
 
 if __name__ == "__main__":
