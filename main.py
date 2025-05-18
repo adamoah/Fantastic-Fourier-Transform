1955e49cb0c80abf6991d227fdc4904176531a26
fliquor <157814367+fliquor@users.noreply.github.com>
2025-05-18 17:22:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def main():
         st.text("There seems to be peaks at certain frequencies of the audio clip. How about we take a look at the frequencies of the notes in the C major scale?" \
                " We've included an audio clip in case you wanted to figure this out using your ears.")
 
-        C4, D4, E4, F4, G4, A4, B4, c% = st.columns(8)
+        C4, D4, E4, F4, G4, A4, B4, C5 = st.columns(8)
         if C4.button("C4", use_container_width=True):
         if D4.button("D4", use_container_width=True):
         if E4.button("E4", use_container_width=True):
