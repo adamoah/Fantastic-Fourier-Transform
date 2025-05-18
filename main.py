112163572c4aee2a094d87373e34529991cc983c
fliquor <157814367+fliquor@users.noreply.github.com>
2025-05-18 19:19:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -197,7 +197,7 @@ def main():
             st.write("This is inside the container")
         
             # You can call any Streamlit command, including custom components:
-            df = pd.DataFrame({'x': curr_graph[0][:curr_graph[1]], 'y': curr_graph[2][:curr_graph[1]]}]
+            df = pd.DataFrame({'x': curr_graph[0][:curr_graph[1]], 'y': curr_graph[2][:curr_graph[1]]})
             fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
             fig.show()
 
